342588e504ea147542e242f1288cc80cf32d4d6f
curtismchale <curtis@curtismchale.c>
2024-08-09 14:29:49 -0700

    big rewrite to use lazy

--- lua/core/plugin_config/coc.lua
+++
@@ -1,3 +0,0 @@
--- setting node 14 for nvim all the time so coc works
-vim.g.coc_node_path = '~/.nvm/versions/node/v16.20.2/bin/node'
-
--- lua/core/plugin_config/gruvbox.lua
+++
@@ -1,4 +0,0 @@
--- setup must be called before loading the colorscheme
-vim.o.termguicolors = true
-vim.o.background = "dark"
-vim.cmd("colorscheme gruvbox")
