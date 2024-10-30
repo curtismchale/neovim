db3df2df6db7de2969eccb2d4de4a8b9839b3297
Curtis McHale <curtis@curtismchale.ca>
2024-10-30 05:59:20 -0700

    changed the warning times for agenda mode

--- lua/plugins/nvimorgmode.lua
+++ lua/plugins/nvimorgmode.lua
@@ -8,6 +8,7 @@ return {
       org_agenda_files = "~/Documents/org/**/*",
       org_default_notes_file = "~/Documents/org/inbox.org",
       org_archive_location = "~/Documents/org/archive/%s",
+      org_deadline_warning_days = 2,
     })
   end,
 }
